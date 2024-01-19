978086fc409821fa649e743e251ef9b568b32da6
tannerru <tanner.rusher@outlook.com>
2024-01-19 08:38:43 -0600

    3

--- VMs/variables.tf
+++ VMs/variables.tf
@@ -26,3 +26,14 @@ variable "cipassword" {
   
 }
 
+variable "proxmox_api_url" {
+  type = string
+}
+
+variable "proxmox_api_token_id" {
+  type = string
+}
+
+variable "proxmox_api_token_secret" {
+  type = string
+}

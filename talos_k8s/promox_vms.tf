267b2ae18a15f0aa9357de6e91f50a6f02a24365
Tannerru <tanner.rusher@outlook.com>
2024-08-05 22:45:41 -0500

    3

--- talos_k8s/promox_vms.tf
+++ talos_k8s/promox_vms.tf
@@ -76,7 +76,6 @@ resource "talos_machine_configuration_apply" "cp_config_apply" {
   client_configuration        = talos_machine_secrets.machine_secrets.client_configuration
   machine_configuration_input = data.talos_machine_configuration.machineconfig_cp.machine_configuration
   }
-  node                        = lookup(var.vms, [for k, v in var.vms : k if contains(v.name, "cp")][0], "ip_address")
 }
 
 # Talos Machine Configuration for Workers
